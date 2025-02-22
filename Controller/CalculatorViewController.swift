1fe46608fa39ac4a2c86c1224bee470acbe66749
Andy Patterson <andyp.dev@proton.me>
2025-02-22 09:19:45 -0600

    [Task] Working on tipChanged action

--- Controller/CalculatorViewController.swift
+++ Controller/CalculatorViewController.swift
@@ -9,13 +9,55 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        if sender == zeroPctButton {
+            zeroPctButton.isSelected = true
+            tenPctButton.isSelected = false
+            twentyPctButton.isSelected = false
+        } else if sender == tenPctButton {
+            tenPctButton.isSelected = true
+            twentyPctButton.isSelected = false
+            zeroPctButton.isSelected = false
 
+        } else if sender == twentyPctButton {
+            twentyPctButton.isSelected = true
+            zeroPctButton.isSelected = false
+            tenPctButton.isSelected = false
+        }
+
+        
+    }
+    
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+        
+    }
+    
+    
+
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
--- Controller/ResultsViewController.swift
+++ Controller/ResultsViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +19,12 @@ class ResultsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+        
+    }
 
+    
     /*
     // MARK: - Navigation
 
