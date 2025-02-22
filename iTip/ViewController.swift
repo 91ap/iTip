c4b1b13bc188b68909c6a2fb5af553acd1896b7f
Andy Patterson <andyp.dev@proton.me>
2025-02-22 05:32:08 -0600

    Initial Commit

---
+++ iTip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTip
+//
+//  Created by apat on 2/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
